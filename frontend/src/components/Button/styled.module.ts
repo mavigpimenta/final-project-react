c5ed4ada1d61dfad8d0a0e03b06988f46589a1a6
JoviOakz <joao.mateussdc@gmail.com>
2024-09-17 16:26:04 -0300

    fix: button component

--- frontend/src/components/Button/styled.module.ts
+++ frontend/src/components/Button/styled.module.ts
@@ -1,7 +1,17 @@
 import styled from "styled-components";
 
 export const StyledButton = styled.button`
-    width: 100%;
-    background-color: greenyellow;
+    width: 400px;
+    padding: 10px;
+    background-color: #007BC0;
     color: white;
+    border: none;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background-color 0.3s ease, box-shadow 0.3s ease;
+
+    &:hover {
+        background-color: #005a87;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    }
 `
