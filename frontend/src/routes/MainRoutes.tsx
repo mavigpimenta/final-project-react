17068b415a588380bafa9a5e9d0af5bc5e0480b6
JoviOakz <joao.mateussdc@gmail.com>
2024-09-18 16:47:49 -0300

    feat: new routes

--- frontend/src/routes/MainRoutes.tsx
+++ frontend/src/routes/MainRoutes.tsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import { LoginPage } from "../pages/LoginPage";
 import EditProfilePage from "../pages/EditProfilePage";
+import RegisterPage from "../pages/RegisterPage";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/updatePass",
     element: <EditProfilePage />,
   }
 ]);
