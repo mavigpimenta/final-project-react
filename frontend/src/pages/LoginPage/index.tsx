f1581a2b09e0e14910a16c115c46f36dd2db6f83
Mavi <mariapimenta2607@gmail.com>
2024-09-18 14:35:09 -0300

    fix: new input

--- frontend/src/pages/LoginPage/index.tsx
+++ frontend/src/pages/LoginPage/index.tsx
@@ -2,14 +2,33 @@ import { useState } from "react";
 import { LoginForm } from "../../components/LoginForm";
 import { ContentWrapper, Logo, PageWrapper } from "./styled.module";
 import LogoImage from '/Logo.png'
+import axios from "axios";
 
 export const LoginPage = () => {
     const [edv, setEdv] = useState('');
     const [password, setPassword] = useState('');
   
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
         e.preventDefault();
-        console.log('Login:', { edv, password });
+    
+        try {
+          const response = await axios.post('http://localhost:8000/user/login', {
+            edv: edv,
+            password: password,
+          });
+    
+          const { token } = response.data;
+    
+          localStorage.setItem('token', token);
+
+          const decodedToken = JSON.parse(atob(token.split('.')[1]));
+          localStorage.setItem('role', decodedToken.role);
+    
+          console.log('Login successful:', response.data);
+          
+        } catch (error) {
+            console.log('An unexpected error occurred. Please try again later.');
+        }
       };
       
     return (
